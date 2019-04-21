4651b0826056a8d54614a7091f9f9688afa69d94
emmanuela <eoa22@njit.edu>
2019-04-21 09:27:18 -0400

    task: completed the thank you page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,24 +2,31 @@
 
 @section('content')
 
-    <h1>Contact</h1>
-    <p class="lead">Use the form to submit your questions.<br> You can follow us on Facebook and Twitter for the up-to-date information.</p>
+    <div class="card at-3 pl-2 pr-2">
 
-    <form action="/contact" method="post">
+        <div class="card-title"><h1>Contact</h1><p class="lead">Use the form to submit your questions.</p></div>
 
-        {{ csrf_field() }}
+            <div class="card-body">
 
-        <div class="form-group">
-            <label for="email">Email address</label>
-            <input type="email" class="form-control" id="email" placeholder="name@example.com">
+
+                <form action="/contact" method="post">
+
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <label for="email">Email Address</label>
+                        <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="body">Message</label>
+                        <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                </form>
+
+            </div>
         </div>
-
-        <div class="form-group">
-            <label for="body">Message</label>
-            <textarea name = "body" class="form-control" id="body" rows="3"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary mb-2">Submit</button>
-    </form>
 
 
 @endsection
