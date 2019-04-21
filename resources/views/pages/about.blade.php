4bcf927b01415516c009a64de5866cdf457e2115
emmanuela <eoa22@njit.edu>
2019-04-21 05:30:07 -0400

    task

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <h1>This is my About page</h1>
+    <p class="lead">Our job is to bring you the latest sport news.<br> You can follow us on Facebook and Twitter for the up-to-date information.</p>
+
+
+
+@endsection
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <h1>Contact</h1>
+    <p class="lead">Use the form to submit your questions.<br> You can follow us on Facebook and Twitter for the up-to-date information.</p>
+
+    <form action="/contact" method="post">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email" placeholder="name@example.com">
+        </div>
+
+        <div class="form-group">
+            <label for="body">Message</label>
+            <textarea name = "body" class="form-control" id="body" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+    </form>
+
+
+@endsection
